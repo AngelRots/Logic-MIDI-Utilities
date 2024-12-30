2eaa2f3880c6cd311103dd2a227c5bc9e7dc40c8
AngelRots <58401916+AngelRots@users.noreply.github.com>
2024-12-30 13:13:05 +0100

    Update Heavenly MIDelay.js

--- Heavenly MIDelay.js
+++ Heavenly MIDelay.js
@@ -1,18 +1,21 @@
-var NeedsTimingInfo = true; 
+//var NeedsTimingInfo = true; 
 
 /* MS-based MIDI Delay with pitched up notes. */
 
 function HandleMIDI(event)
 {
 
- var daw = GetTimingInfo()
+ //var daw = GetTimingInfo()
+ var paramExt1 = GetParameter("Incoming Note Pitch")
+ var paramExt2 = GetParameter("Delay Amount")
 
 	event.trace();
 	event.send();
 	if(event instanceof Note)
 	{
-	event.pitch += GetParameter("Incoming Note Pitch"); 
-	event.sendAfterMilliseconds(GetParameter("Delay Amount"))
+	event.pitch += paramExt1;
+	event.sendAfterMilliseconds(paramExt2);
+	//event.trace();
 
 	
 	}
@@ -20,10 +23,11 @@ function HandleMIDI(event)
 //----------------------------------------------------
 var PluginParameters = [
 
-{name:"Delay Amount", type:"log", numberOfSteps:100,
+{name:"Delay Amount", type:"log", numberOfSteps:2000,
 minValue:0, maxValue: 2000, unit:"ms", defaultValue:500},
 
 {name:"Incoming Note Pitch", type:"lin", numberOfSteps:16,
 minValue:0, maxValue: 24, defaultValue:7},
 
 ];
+
