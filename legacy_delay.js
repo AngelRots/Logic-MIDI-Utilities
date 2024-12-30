622ac4c6d040581cb2820519964745787bf43c91
AngelRots <58401916+AngelRots@users.noreply.github.com>
2024-12-30 14:00:04 +0100

    Create legacy_delay.js

---
+++ legacy_delay.js
@@ -0,0 +1,32 @@
+//var NeedsTimingInfo = true; 
+
+/* MS-based MIDI Delay with pitched up notes. */
+
+function HandleMIDI(event)
+{
+
+ //var daw = GetTimingInfo()
+ var paramExt1 = GetParameter("Incoming Note Pitch")
+ var paramExt2 = GetParameter("Delay Amount")
+
+	event.trace();
+	event.send();
+	if(event instanceof Note)
+	{
+	event.pitch += paramExt1;
+	event.sendAfterMilliseconds(paramExt2);
+	//event.trace();
+
+	
+	}
+}
+//----------------------------------------------------
+var PluginParameters = [
+
+{name:"Delay Amount", type:"log", numberOfSteps:2000,
+minValue:0, maxValue: 2000, unit:"ms", defaultValue:500},
+
+{name:"Incoming Note Pitch", type:"lin", numberOfSteps:16,
+minValue:0, maxValue: 24, defaultValue:7},
+
+];
